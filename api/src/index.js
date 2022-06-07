ae43a19ffa1e69e484519fea7578235d86fc6039
kaue pereira santos <kaueps.15@gmail.com>
2022-06-07 09:24:23 -0300

    novaa

--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,13 @@
 import "dotenv/config"
 
-import express from 'dotenv'
+import heroicontroller from './controller/heroicontroller.js'
+import express from "express"
+import cors from 'cors'
+
+const server = express();
+server.user(cors());
+server.user(express.json());
+
+server.user (heroicontroller);
+server.listen(process.env.PORT,()=>
+console.log(`api online na porta ${process.env.PORT}`)); 
